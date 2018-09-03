575d78ebae33039acf9d9ce353b9c2f709109d04
kochetkov <kochetkov@cg.ru>
2018-09-03 14:51:19 +0300

    Revert "SUPPORT-1477_change_correlation_column_types"
    This reverts commit 4f6aee636c3d8e67755deba1b48e2d3bdafdbf1b.

--- jbpm-persistence/jbpm-persistence-jpa/src/main/java/org/jbpm/persistence/correlation/CorrelationKeyInfo.java
+++ jbpm-persistence/jbpm-persistence-jpa/src/main/java/org/jbpm/persistence/correlation/CorrelationKeyInfo.java
@@ -50,8 +50,7 @@ public class CorrelationKeyInfo implements PersistentCorrelationKey, Serializabl
     private int version;
     
     private long processInstanceId;
-
-    @Column(columnDefinition = "text")
+    
     private String name;
     
     @OneToMany(mappedBy="correlationKey", cascade=CascadeType.ALL)
